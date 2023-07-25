2bab88c4c7deb571f1e13984a3e97299899c4e46
NickUCLA <133281012+NickUCLA@users.noreply.github.com>
2023-07-24 20:03:40 -0700

    added game loop and start of enemy controller

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,25 @@
-console.log("test");
+// This controller allows for the modules of js to be imported and used
+import EnemyController from "./EnemyController.js";
+
+const canvas = document.getElementById("game");
+// ctx allows you to draw and manipulate 2D graphics (you pass 2D as an argument for 2 demensional drawings)
+const ctx = canvas.getContext("2d");
+
+//sets the size of canvas
+canvas.width = 600;
+canvas.height = 600;
+
+const background = new Image();
+
+// sets background for canvas
+background.src = "assets/images/ocean-background.jpg";
+
+const enemyController = new EnemyController(canvas);
+
+function game() {
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  enemyController.draw(ctx);
+}
+
+//this interval makes the game fucntion execute 60 times every sec (60fps)
+setInterval(game, 1000 / 60);
