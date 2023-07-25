52c150c1d9bbb9686cd4e3dee6bdfcd58dc4e7d2
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-07-24 19:56:16 -0700

    drew player and creating walls

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
 console.log("test");
+//
+// import Player from "../Player";
+
+
+
+
+
+
+// const player = new Player(canvas, 3);
+
+
+//Add to function game() after enemycontroller
+//player.draw(ctx);
